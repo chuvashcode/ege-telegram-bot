9f7b7fb54aafd313b4b6e704486e4073d77aab21
Roman Timoshkin <roma-timoshkin@mail.ru>
2022-10-14 18:41:49 +0400

    added verb interaction to database

--- database.py
+++ database.py
@@ -10,18 +10,21 @@ class WordsDB:
         self.adjectives: list = []
         self.adparticiples: list = []
         self.participles: list = []
+        self.verbs: list = []
 
     def parse(self) -> None:
         with open("./db/nouns.csv", "r", encoding="utf8") as nouns, \
                 open("./db/adverbs.csv", "r", encoding="utf8") as adverbs, \
                 open("./db/adjectives.csv", "r", encoding="utf8") as adjectives, \
                 open("./db/adparticiples.csv", "r", encoding="utf8") as adparticiples, \
-                open("./db/participles.csv", "r", encoding="utf8") as participles:
+                open("./db/participles.csv", "r", encoding="utf8") as participles, \
+                open("./db/verbs.csv", "r", encoding="utf8") as verbs:
             noun_reader = csv.reader(nouns, delimiter=",")
             adverb_reader = csv.reader(adverbs, delimiter=",")
             adjective_reader = csv.reader(adjectives, delimiter=",")
             adparticiple_reader = csv.reader(adparticiples, delimiter=",")
             participle_reader = csv.reader(participles, delimiter=",")
+            verb_reader = csv.reader(verbs, delimiter=",")
             for noun in noun_reader:
                 self.nouns = noun
             for adverb in adverb_reader:
@@ -32,9 +35,11 @@ class WordsDB:
                 self.adparticiples = adparticiple
             for participle in participle_reader:
                 self.participles = participle
+            for verb in verb_reader:
+                self.verbs = verb
 
     def get_random(self) -> str:
-        all_words: list = self.nouns + self.adverbs + self.adjectives + self.participles + self.adparticiples
+        all_words: list = self.nouns + self.adverbs + self.adjectives + self.participles + self.adparticiples + self.verbs
         random_word: str = random.choice(all_words)
         return random_word
 
@@ -57,3 +62,8 @@ class WordsDB:
     def get_random_participle(self) -> str:
         random_participle: str = random.choice(self.participles)
         return random_participle
+
+    def get_random_verb(self) -> str:
+        random_verb: str = random.choice(self.verbs)
+        return random_verb
+
